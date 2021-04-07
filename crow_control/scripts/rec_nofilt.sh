f58bdc162562239176e6a05ee386bedf7181a843
Your Name <you@example.com>
2021-04-07 14:04:26 +0200

    Added recording script

---
+++ crow_control/scripts/rec_nofilt.sh
@@ -0,0 +1,29 @@
+ros2 bag record \
+    /camera1/aligned_depth_to_color/camera_info \
+    /camera1/aligned_depth_to_color/image_raw \
+    /camera1/color/camera_info \
+    /camera1/color/image_raw \
+    /camera1/depth/camera_info \
+    /camera1/depth/color/points \
+    /camera1/depth/image_rect_raw \
+    /camera1/detections/bboxes \
+    /camera1/detections/image_annot \
+    /camera1/detections/masks \
+    /camera1/detections/segmented_pointcloud \
+    /camera1/detections/segmented_pointcloud_debug \
+    /camera1/extrinsics/depth_to_color \
+    /camera2/aligned_depth_to_color/camera_info \
+    /camera2/aligned_depth_to_color/image_raw \
+    /camera2/color/camera_info \
+    /camera2/color/image_raw \
+    /camera2/depth/camera_info \
+    /camera2/depth/color/points \
+    /camera2/depth/image_rect_raw \
+    /camera2/detections/bboxes \
+    /camera2/detections/image_annot \
+    /camera2/detections/masks \
+    /camera2/detections/segmented_pointcloud \
+    /camera2/detections/segmented_pointcloud_debug \
+    /camera2/extrinsics/depth_to_color \
+    /tf \
+    /tf_static
