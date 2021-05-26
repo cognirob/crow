a32a448a3440bb61e54808ea3bf4179b580a736d
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2021-05-26 18:57:41 +0200

    Added documentation

--- crow_control/crow_control/utils/param_client.py
+++ crow_control/crow_control/utils/param_client.py
@@ -6,14 +6,6 @@ Created on Wed May 26 14:56:16 2021
 @author: syxtreme
 """
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 25 14:19:55 2021
-
-@author: syxtreme
-"""
-
 import zmq
 from threading import Thread
 from warnings import warn
@@ -22,78 +14,129 @@ from zmq.utils.strtypes import asbytes
 
 
 class ParamClient():
-    
+
     def __init__(self, start_port=25652, addr="127.0.0.1", protocol="tcp"):
+        """ Creates a client for parameters. The client can subscribe to various parameters
+        and then it will receive updates about changes to these parameters.
+        The parameters can be accessed as if they were properties of this client.
+        ***WARNING***
+        The property voodoo does changes to the base class itself. That means,
+        using more clients in the same script will be buggy - all instances will have the same
+        properties. Although, this should be ok in most cases, it is recommended to find another way.
+
+        Args:
+            start_port (int, optional): A port where the parameters server operates. This is a port on which
+            only the publisher will send parameter updates. Parameter changes and definitions/declarations
+            will be done over ports with numbers +3 and +7 higher. Defaults to 25652.
+            addr (str, optional): IP address on which the server operates. Defaults to "127.0.0.1".
+            protocol (str, optional): Protocol to use for communication.
+        """
         self.__context = zmq.Context()
-            
+
         # subscirbe to parameter changes
         self.__addr_pub = f"{protocol}://{addr}:{str(start_port)}"
-        self.subscriber = self.__context.socket(zmq.SUB)
-        self.subscriber.connect(self.__addr_pub)
-        
+        self.__subscriber = self.__context.socket(zmq.SUB)
+        self.__subscriber.connect(self.__addr_pub)
+
         # socket for param change requests
         self.__addr_ch = f"{protocol}://{addr}:{str(start_port + 3)}"
-        self.changer = self.__context.socket(zmq.REQ)
-        self.changer.connect(self.__addr_ch)
-        
+        self.__changer = self.__context.socket(zmq.REQ)
+        self.__changer.connect(self.__addr_ch)
+
         # socket for param definition
         self.__addr_def = f"{protocol}://{addr}:{str(start_port + 7)}"
-        self.definer = self.__context.socket(zmq.REQ)
-        self.definer.connect(self.__addr_def)
-        
+        self.__definer = self.__context.socket(zmq.REQ)
+        self.__definer.connect(self.__addr_def)
+
         self.__params = {}
         self.active = True
-        
+
         self.poller_thread = Thread(target=self.__poll, daemon=True)
         self.poller_thread.start()
 
     def subscribe(self, param):
+        """Subscribe to parameter updates. This does not guarantee that the parameter
+        exists and has any value. This only tells the server that this client
+        wants to be notified if a parameter with the specified name changes.
+        At the begining, the value of the parameter will be "None" and will remain so
+        until this client receives value update for the parameter. This will occur
+        rather quickly if the parameter exists on the server but this is not known
+        at the time this method is called. This should be fine, just be aware of it.
+
+        Args:
+            param (str): The name of the parameter
+        """
         if param in self.__params:
             return
-        
+
         self.__params[param] = None
         setattr(ParamClient, param, property(
                 lambda self, param=param: self.__params[param],
                 lambda self, value, param=param: self.__set_param(param, value)
             ))
-        self.subscriber.setsockopt(zmq.SUBSCRIBE, param.encode('utf-8'))
-        
+        self.__subscriber.setsockopt(zmq.SUBSCRIBE, param.encode('utf-8'))
+
     def declare(self, param, default_value=None):
+        """Subsribes to parameter updates. This does roughly the same as
+        subscribe but presents a default value to fill in if the parameter
+        does not exist on the server. Use this if you want to always start with some
+        (not None) value but you don't want to overwrite any existing values.
+        E.g., you need the param to be False at the beginning but remain True
+        if it was set to True by other client (with the subscribe method,
+        it would not be False but None, if it was not defined, yet).
+
+        Args:
+            param (str): The name of the parameter
+            default_value (any, optional): Default value for the parameter if it was not
+            defined, yet. Defaults to None.
+        """
         self.__send_definition(param, default_value, 0)
-        
+
     def define(self, param, value):
+        """Subscribes to parameter updates. Unlike subscribe or declare, this method always
+        overwrites the current parameter value. That is, whether the parameter exists on the server
+        or not, after calling this method, it will have value set to "value".
+        Use this method for clients that should "own" the control over this parameter.
+
+        Args:
+            param (str): The name of the parameter.
+            value (any): The starting or new value of the parameter. The parameter is always
+            set to this value upon definition.
+        """
         self.__send_definition(param, value, 1)
-        
+
+    def destroy(self):
+        """Stops the updates and closes all connection.
+        """
+        self.active = False
+        self.__subscriber.close()
+        self.__changer.close()
+        self.__definer.close()
+
     def __send_definition(self, param, value, overwrite):
         if param in self.__params:
             return
-        
-        self.definer.send_multipart([param.encode('utf-8'), cpl.dumps(value), asbytes(chr(overwrite))])
-        response = self.definer.recv().decode()
+
+        self.__definer.send_multipart([param.encode('utf-8'), cpl.dumps(value), asbytes(chr(overwrite))])
+        response = self.__definer.recv().decode()
         if response == "false":
             raise AttributeError(f"Could not declare the parameter {param} (value={value}).")
 
         self.subscribe(param)
-        
-    def destroy(self):
-        self.active = False
-        self.subscriber.close()
-        self.changer.close()
-        self.definer.close()
-        
+
     def __set_param(self, param, value):
-        self.changer.send_multipart([param.encode('utf-8'), cpl.dumps(value)])
-        response = self.changer.recv().decode()
+        self.__changer.send_multipart([param.encode('utf-8'), cpl.dumps(value)])
+        response = self.__changer.recv().decode()
         # print(response)
         if response == "true":
             self.__params[param] = value
-    
+
     def __poll(self):
         while self.active:
-            param, msg = self.subscriber.recv_multipart()
+            param, msg = self.__subscriber.recv_multipart()
             # print(param, msg)
             self.__params[param.decode()] = cpl.loads(msg)
-        
+
 
 
 
--- crow_control/crow_control/utils/param_server.py
+++ crow_control/crow_control/utils/param_server.py
@@ -13,70 +13,91 @@ import cloudpickle as cpl
 
 
 class ParamServer():
-    
+
     def __init__(self, start_port=25652, addr="127.0.0.1", protocol="tcp"):
+        """ Creates a server for parameters. ParamClient nodes can subscribe to parameter updates
+        handled via this server. If one client changes a parameter, other clients receive and update.
+
+        Args:
+            start_port (int, optional): A port on which this server operates. This is a port on which
+            only the publisher will send parameter updates. Client parameter updates and definitions/declarations
+            will be done over ports with numbers +3 and +7 higher. Defaults to 25652.
+            addr (str, optional): IP address on which this server operates. See PyZMQ documentation
+            for various options (e.g. "localhost", "*", etc.). Defaults to "127.0.0.1".
+            protocol (str, optional): Protocol to use for communication. Probably don't change this,
+            unless you really know what you are doing. Defaults to "tcp".
+        """
         self.__context = zmq.Context()
-            
+
         # socket to publish parameters
         self.__addr_pub = f"{protocol}://{addr}:{str(start_port)}"
         self.publisher = self.__context.socket(zmq.XPUB)
         self.publisher.setsockopt(zmq.XPUB_VERBOSE, True)
         self.publisher.bind(self.__addr_pub)
-        
+
         # socket for param change requests
         self.__addr_ch = f"{protocol}://{addr}:{str(start_port + 3)}"
         self.changer = self.__context.socket(zmq.REP)
         self.changer.bind(self.__addr_ch)
-        
+
         # socket for param definition
         self.__addr_def = f"{protocol}://{addr}:{str(start_port + 7)}"
         self.definer = self.__context.socket(zmq.REP)
         self.definer.bind(self.__addr_def)
-        
+
         self.poller = zmq.Poller()
         self.poller.register(self.publisher, zmq.POLLIN)
         self.poller.register(self.changer, zmq.POLLIN)
-        self.poller.register(self.definer, zmq.POLLIN) 
-        
+        self.poller.register(self.definer, zmq.POLLIN)
+
         self.__params = {}
         self.active = True
-        
+
         self.poller_thread = Thread(target=self.__poll, daemon=True)
         self.poller_thread.start()
 
-        
+
     def define(self, param, default_value=None):
+        """Create a definition for the given parameter. This is not necessary,
+        the clients themselves can define/declare the parameters.
+
+        Args:
+            param (str): The parameter name.
+            default_value (any, optional): Value to which the parameter should be set to. Defaults to None.
+        """
         if param in self.__params:
             return
             # warn(f"Parameter {param} aleardy defined and somebody tries to redefine it!")
-        
+
         setattr(ParamServer, param, property(
                 lambda self, param=param: self.__params[param],
                 lambda self, value, param=param: self.__set_param(param, value)
             ))
-        
+
         self.__set_param(param, default_value)
-        
+
     def destroy(self):
+        """Stop the threaded poller and close all connections.
+        """
         self.active = False
         self.publisher.close()
         self.changer.close()
         self.definer.close()
-        
+
     def __set_param(self, param, value):
         self.__params[param] = value
         self.__broadcast_param(param)
-        
+
     def __broadcast_param(self, param):
         self.publisher.send_multipart([param.encode('utf-8'), cpl.dumps(self.__params[param])])
-    
+
     def __poll(self):
         while self.active:
             try:
                 socks = dict(self.poller.poll())
             except zmq.error.ZMQError:
                 break
-        
+
             if self.publisher in socks:  # handle new subscriptions
                 msg = self.publisher.recv()
                 sub, param = msg[0], msg[1:].decode()
@@ -96,7 +117,7 @@ class ParamServer():
                     self.changer.send(b"true")
                 else:
                     self.changer.send(b"false")
-        
+
             if self.definer in socks:  # handle new param definitions here
                 param, value, overwrite = self.definer.recv_multipart()
                 param = param.decode()
