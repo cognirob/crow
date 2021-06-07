14825f16c69a3d603ae1f7578eb1a8e7612144d3
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2021-06-07 12:46:30 +0200

    Fixed wrong poping direction

--- crow_control/crow_control/utils/queue_server.py
+++ crow_control/crow_control/utils/queue_server.py
@@ -14,34 +14,34 @@ from collections import deque
 
 
 class QueueServer():
-    
+
     def __init__(self, maxlen=-1, queue_name="queue", start_port=12821, addr="127.0.0.1", protocol="tcp"):
         self.__queue_name = queue_name.encode('utf-8')
         self.__context = zmq.Context()
-            
+
         # socket to publish parameters
         self.__addr_pub = f"{protocol}://{addr}:{str(start_port)}"
         self.__publisher = self.__context.socket(zmq.PUB)
         self.__publisher.bind(self.__addr_pub)
         self.buffer = deque(maxlen=maxlen)
-        
+
     def append(self, data):
         self.buffer.append(data)
         self.__broadcast()
 
     def pop(self):
-        data = self.buffer.pop()
+        data = self.buffer.popleft()
         self.__broadcast()
         self.__broadcast_popped(data)
         return data
-    
+
     def remove(self, index):
         del self.buffer[index]
-        self.__broadcast()        
+        self.__broadcast()
 
     def destroy(self):
         del self.buffer
-        self.publisher.close()
+        self.__publisher.close()
 
     def find_name_index(self, name):
         for pos, t in enumerate(self.buffer):
@@ -51,10 +51,10 @@ class QueueServer():
 
     def __broadcast(self):
         self.__publisher.send_multipart([self.__queue_name + b'.update', cpl.dumps(self.buffer)])
-        
+
     def __broadcast_popped(self, data):
         self.__publisher.send_multipart([self.__queue_name + b'.popped', cpl.dumps(data)])
-        
+
     def __len__(self):
         return len(self.buffer)
-        
+
