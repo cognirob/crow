ce7b9625696a21df254996692f7d146dc86e1151
Your Name <you@example.com>
2021-11-22 18:53:31 +0100

    Add multiple request to search for objects.

--- crow_control/crow_control/assembly_planner.py
+++ crow_control/crow_control/assembly_planner.py
@@ -30,6 +30,7 @@ class AssemblyPlanner(Node):
     START_BUILD_SERVICE = 'assembly_start'
     CANCEL_BUILD_SERVICE = 'assembly_cancel'
     RESET_OBJECTS_SERVICE = "/reset_tracker_objects"
+    MAX_OBJ_REQUEST_TIME = 2
 
     def __init__(self):
         super().__init__("assembly_planner")
@@ -58,21 +59,25 @@ class AssemblyPlanner(Node):
         self.templ_det = self.ui.load_file('templates_detection.json')
         self.obj_det = self.ui.load_file('objects_detection.json')
         self.guidance_file = self.ui.load_file('guidance_dialogue.json')
+        self.nl_processor = NLProcessor(language=self.LANG, client=self.crowracle)
+
         self.pclient = ParamClient()
-        self.pclient.define("processor_busy_flag", False) # State of the sentence processor
-        self.pclient.define("halt_nlp", False) # If true, NLP input should be halted
-        self.pclient.define("silent_mode", 1) # Set by the user (level of the talking - 1 Silence, 2 - Standard speech, 3 - Debug mode/Full speech)
-        self.pclient.define("ready_for_next_sentence", True) # If true, sentence processor can process and send next command
-        self.pclient.define("can_start_talking", True) # If true, can start playing buffered sentences
-        self.pclient.define("det_obj", "-")
-        self.pclient.define("det_command", "-")
-        self.pclient.define("det_obj_name", "-")
-        self.pclient.define("det_obj_in_ws", "-")
-        self.pclient.define("status", "-")
+        self.pclient.declare("processor_busy_flag", False) # State of the sentence processor
+        self.pclient.declare("halt_nlp", False) # If true, NLP input should be halted
+        self.pclient.declare("silent_mode", 1) # Set by the user (level of the talking - 1 Silence, 2 - Standard speech, 3 - Debug mode/Full speech)
+        self.pclient.declare("ready_for_next_sentence", True) # If true, sentence processor can process and send next command
+        self.pclient.declare("can_start_talking", True) # If true, can start playing buffered sentences
+        self.pclient.declare("det_obj", "-")
+        self.pclient.declare("det_command", "-")
+        self.pclient.declare("det_obj_name", "-")
+        self.pclient.declare("det_obj_in_ws", "-")
+        self.pclient.declare("status", "-")
         self.pclient.define("building_in_progress", False)
 
         self.objects = [o[2:].lower() for o in sorted(dir(AssemblyObjectProbability)) if o.startswith("O_")]
         self.actions = [a[2:].lower() for a in sorted(dir(AssemblyActionProbability)) if a.startswith("A_")]
+
+        self.received_next_object = False
 
         # callbacks
         self.create_subscription(AssemblyActionProbability, self.ASSEMBLY_ACTION_TOPIC, self.action_cb, 10)
@@ -216,12 +221,15 @@ class AssemblyPlanner(Node):
             self.ui.buffered_say(self.guidance_file[self.LANG]["assembly_finished"], say=2)
             self.wait_then_talk()
         [next_node, self.obj_to_add] = self.am.detect_next_state(self.gp, self.max_node)
+        self.received_next_object = True
         self.send_request_to_robot()
 
     def send_request_to_robot(self):
         if self.obj_to_add == 'Peg':
                 # self.max_node_prev = self.max_node
                 print('human should pick up the peg')
+        elif self.obj_to_add.lower() == 'othero':
+                print('Skipping "Other object".')
         elif self.max_node == self.max_node_prev:
             print('same node as previously. No action.')
         else:
@@ -238,64 +246,85 @@ class AssemblyPlanner(Node):
                 input_sentence = 'put '+ obj_to_add_lang + " on the table"
             else:
                 input_sentence = 'polož '+ obj_to_add_lang + " na stůl"
-            print(input_sentence)
-            input_sentence = input_sentence.lower()
-            nl_processor = NLProcessor(language=self.LANG, client=self.crowracle)
-            program_template = nl_processor.process_text(input_sentence)
-            print()
-            print("Program Template")
-            print("--------")
-            print(program_template)
-            robot_program = self.run_program(program_template)
-            data = []
-            try:
-                template = program_template.root.children[0].template
-                # if (template is None) or (not template.is_filled()):
-                #     self.wait_then_talk()
-                #     continue
-                dict1 = template.get_inputs()
-                template_type = dict1.get('action_type', '-')
-                target_obj = dict1.get('target')
-                if target_obj is not None:
-                    if hasattr(target_obj, "flags") and 'last_mentioned' in target_obj.flags:
-                        # TODO add last mentioned correferenced object and then delete this and adjust in ObjectGrounder
-                        target_obj = None
-                        dict1['target'] = None
-                if target_obj is not None:
-                    if len(target_obj) > 1:
-                        self.wait_then_talk()
-                    # @TODO: ask for specification, which target_obj to choose
-                    object_detected = target_obj
-                    found_in_ws = True
-                else:
-                    object_detected = '-'
-                    found_in_ws = False
 
-                self.send_status("zpracovavam", template_type, object_detected, found_in_ws)
-                if found_in_ws:
-                    if dict1.get('action', False):
-                        data.append(dict1)
-                        actions = json.dumps(data)
-                        msg = StampedString()
-                        msg.header.stamp = self.get_clock().now().to_msg()
-                        msg.data = actions
-                        print(f'will publish {msg.data}')
-                # {"action": 64, "action_type": "uka\u017e", "target_type": "onto_uri",
-                #  "target": ["http://imitrob.ciirc.cvut.cz/ontologies/crow#cube_holes_1",
-                #             "http://imitrob.ciirc.cvut.cz/ontologies/crow#cube_holes_2",
-                #             "http://imitrob.ciirc.cvut.cz/ontologies/crow#cube_holes_3"],
-                #  "target_ph_cls": "http://imitrob.ciirc.cvut.cz/ontologies/crow#Cube"}
-                        self.assembly_publisher.publish(msg)
-                        self.send_status("pozadavek odeslan", template_type, object_detected, found_in_ws)
-                        self.wait_then_talk()
-                        self.pclient.processor_busy_flag = False
+            error_happened = False
+            found_in_ws = False
+            success = False
+            template = None
+            self.received_next_object = False
+
+            # print(input_sentence)
+            input_sentence = input_sentence.lower()
+            program_template = self.nl_processor.process_text(input_sentence)
+            print("--------")
+            print("Program Template")
+            print(program_template)
+
+            start_attempt_time = time.time()
+            while not self.received_next_object and (time.time() - start_attempt_time) < self.MAX_OBJ_REQUEST_TIME:
+                robot_program = self.run_program(program_template)
+                data = []
+                try:
+                    template = program_template.root.children[0].template
+                    if not template.is_filled():
+                        found_in_ws = False
+                        continue
+                    dict1 = template.get_inputs()
+                    template_type = dict1.get('action_type', '-')
+                    target_obj = dict1.get('target')
+                    if target_obj is not None:
+                        if hasattr(target_obj, "flags") and 'last_mentioned' in target_obj.flags:
+                            # TODO add last mentioned correferenced object and then delete this and adjust in ObjectGrounder
+                            target_obj = None
+                            dict1['target'] = None
+                    if target_obj is not None:
+                        if len(target_obj) > 1:
+                            self.wait_then_talk()
+                        # @TODO: ask for specification, which target_obj to choose
+                        object_detected = target_obj
+                        found_in_ws = True
+                        success = True
+                        break
                     else:
-                        self.send_status("neznamy")
-                        self.wait_then_talk()
-            except AttributeError as  e:
-                print('No template found error')
-                self.send_status("neznamy prikaz")
-                self.wait_then_talk()
+                        object_detected = '-'
+                        found_in_ws = False
+                    # success = True
+                    # break
+                except AttributeError as  e:
+                    print('No template found error')
+                    self.send_status("neznamy prikaz")
+                    self.wait_then_talk()
+                    error_happened = True
+                    break
+                # print(f"Delay at the end: {time.time() - start_attempt_time}")
+
+            if found_in_ws and not error_happened:
+                self.send_status("zpracovavam", template_type, object_detected, found_in_ws)
+                if dict1.get('action', False):
+                    data.append(dict1)
+                    actions = json.dumps(data)
+                    msg = StampedString()
+                    msg.header.stamp = self.get_clock().now().to_msg()
+                    msg.data = actions
+                    print(f'will publish {msg.data}')
+            # {"action": 64, "action_type": "uka\u017e", "target_type": "onto_uri",
+            #  "target": ["http://imitrob.ciirc.cvut.cz/ontologies/crow#cube_holes_1",
+            #             "http://imitrob.ciirc.cvut.cz/ontologies/crow#cube_holes_2",
+            #             "http://imitrob.ciirc.cvut.cz/ontologies/crow#cube_holes_3"],
+            #  "target_ph_cls": "http://imitrob.ciirc.cvut.cz/ontologies/crow#Cube"}
+                    self.assembly_publisher.publish(msg)
+                    self.send_status("pozadavek odeslan", template_type, object_detected, found_in_ws)
+                    self.wait_then_talk()
+                    self.pclient.processor_busy_flag = False
+                else:
+                    self.send_status("neznamy")
+                    self.wait_then_talk()
+            else:
+                if hasattr(template, "target_ph_cls"):
+                    self.get_logger().warn(f'Object of class {template.target_ph_cls} not found in workspace, quitting.')
+                else:
+                    self.get_logger().error(f'Failed to process command {input_sentence}')
+
         self.max_node_prev = self.max_node
 
     def run_program(self, program_template):
@@ -327,13 +356,14 @@ def main():
     ap = AssemblyPlanner()
     try:
         n_threads = 2
-        mte = MultiThreadedExecutor(num_threads=n_threads, context=rclpy.get_default_context())
-        rclpy.spin_once(ap, executor=mte)
-        ap.get_logger().info("ready")
-        rclpy.spin(ap, executor=mte)
+        # mte = MultiThreadedExecutor(num_threads=n_threads, context=rclpy.get_default_context())
+        # rclpy.spin_once(ap, executor=mte)
+        # ap.get_logger().info("ready")
+        # rclpy.spin(ap, executor=mte)
+        rclpy.spin(ap)
         ap.destroy_node()
     except KeyboardInterrupt:
-        ap.destroy_node()
+        # ap.destroy_node()
         print("User requested shutdown.")
     except BaseException as e:
         print(f"Some error had occured: {e}")
